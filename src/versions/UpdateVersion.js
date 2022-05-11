5dc84f22131fb03a92c9690f073e0f5d85735ed0
noelmoriano <noel.moriano@webuild.pe>
2022-05-11 11:36:52 -0500

    upgrade versions

--- src/versions/UpdateVersion.js
+++ src/versions/UpdateVersion.js
@@ -1,12 +1,49 @@
 import React from "react";
+import { Button } from "../components/ui";
+import Title from "antd/lib/typography/Title";
+import Col from "antd/lib/col";
+import Row from "antd/lib/row";
+import styled from "styled-components";
 
 export const UpdateVersion = () => {
   return (
-    <section>
-      <h1>Please refresh to get the latest version of the application.</h1>
-      <button type="primary" onClick={() => document.location.reload(true)}>
-        REFRESH
-      </button>
-    </section>
+    <Container>
+      <WrapperRow gutter={8}>
+        <Col span={24}>
+          <Wrapper>
+            <Title level={2}>
+              Actualice para obtener la última versión de la aplicación.
+            </Title>
+            <Button
+              type="primary"
+              size="large"
+              onClick={() => document.location.reload(true)}
+              style={{ margin: "auto" }}
+            >
+              ACTUALIZAR
+            </Button>
+          </Wrapper>
+        </Col>
+      </WrapperRow>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+const WrapperRow = styled(Row)`
+  width: 100%;
+  height: 100%;
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
