156af857737dfb83660d4d4103629ba1587f35be
noelmoriano <noel.moriano@webuild.pe>
2022-05-22 11:50:27 -0500

    cleaning contacts files

--- src/pages/contacts/_contactId/index.js
+++ src/pages/contacts/_contactId/index.js
@@ -1,78 +1,42 @@
-// import React, { useEffect, useState } from "react";
-// import Row from "antd/lib/row";
-// import Col from "antd/lib/col";
-// import { useHistory, useParams } from "react-router";
-// import Title from "antd/es/typography/Title";
-// import { Tabs } from "antd";
-// import { AnimeInformation } from "./AnimeInformation";
-// import { AnimeEpisodes } from "./AnimeEpisodes";
-// import { firestore } from "../../../firebase";
-// import { useDefaultFirestoreProps } from "../../../hooks";
-// import { Spinner } from "../../../components/ui";
-// import { capitalize, get, isEmpty, startCase } from "lodash";
-//
-// const { TabPane } = Tabs;
-//
-// export const Anime = () => {
-//   const { animeId } = useParams();
-//
-//   const [anime, setAnime] = useState({});
-//   const [loadingAnime, setLoadingAnime] = useState(true);
-//
-//   const history = useHistory();
-//
-//   const { assignCreateProps } = useDefaultFirestoreProps();
-//
-//   useEffect(() => {
-//     fetchAnime();
-//   }, [animeId]);
-//
-//   const fetchAnime = async () => {
-//     try {
-//       if (animeId === "new")
-//         return setAnime(
-//           assignCreateProps({ id: firestore.collection("animes").doc().id })
-//         );
-//
-//       const animeQuery = await firestore
-//         .collection("animes")
-//         .doc(animeId)
-//         .get();
-//
-//       const anime_ = await animeQuery.data();
-//
-//       if (isEmpty(anime_)) return history.push("/animes");
-//
-//       setAnime(anime_);
-//     } catch (e) {
-//       console.log("[Error fetch anime]->", e);
-//       history.push("/animes");
-//     } finally {
-//       setLoadingAnime(false);
-//     }
-//   };
-//
-//   const navigateTo = (url) => history.push(url);
-//
-//   if (loadingAnime || !anime) return <Spinner height="50vh" />;
-//
-//   return (
-//     <Row>
-//       <Col span={24}>
-//         <Title level={2}>
-//           {startCase(capitalize(get(anime, "name", "-")))}
-//         </Title>
-//       </Col>
-//       <Col span={24}>
-//         <Tabs type="card">
-//           <TabPane tab="Información" key="1">
-//             <AnimeInformation anime={anime} />
-//           </TabPane>
-//           <TabPane tab="Episodios" key="2" disabled={animeId === "new"}>
-//             <AnimeEpisodes anime={anime} navigateTo={navigateTo} />
-//           </TabPane>
-//         </Tabs>
-//       </Col>
-//     </Row>
-//   );
-// };
+import React from "react";
+import Row from "antd/lib/row";
+import Col from "antd/lib/col";
+import Timeline from "antd/lib/timeline";
+import { useParams } from "react-router";
+
+export const Contact = () => {
+  const { contactId } = useParams();
+
+  // const [contact, , errorContact] = useDocumentDataOnce(
+  //   firestore.collection("contacts").doc(contactId)
+  // );
+  //
+  // console.log("contact->", contact);
+
+  return (
+    <Row>
+      <Col span={24}>
+        <h1>{contactId}</h1>
+      </Col>
+      <Col span={24}>
+        <Timeline mode="alternate">
+          <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
+          <Timeline.Item color="green">
+            Solve initial network problems 2015-09-01
+          </Timeline.Item>
+          <Timeline.Item>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo.
+          </Timeline.Item>
+          <Timeline.Item color="red">
+            Network problems being solved 2015-09-01
+          </Timeline.Item>
+          <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
+          <Timeline.Item>Technical testing 2015-09-01</Timeline.Item>
+        </Timeline>
+      </Col>
+    </Row>
+  );
+};
